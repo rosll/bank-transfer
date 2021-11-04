14e319e254a9dae6f3187d3320e1bb0fb2ccd6a1
Romulo Lima <romulosilva-07@hotmail.com>
2021-11-03 22:09:31 -0300

    atribuído a opção de inserir nova conta

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Bank.Transfer.Classes;
 using Bank.Transfer.Enum;
 
@@ -6,11 +7,92 @@ namespace Bank.Transfer
 {
     class Program
     {
+        static List<Conta> listContas = new List<Conta>();
         static void Main(string[] args)
         {
-            Conta minhaConta = new Conta(TipoConta.PessoaFisica, 0, 0, "Romulo Lima");
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        //ListarConta();
+                        break;
+                    
+                    case "2":
+                        InserirConta();
+                        break;
+                    
+                    case "3":
+                        // Transferir();
+                        break;
+                    
+                    case "4":
+                        //Sacar();
+                        break;
+                    
+                    case "5":
+                        // Depositar();
+                        break;
+                    
+                    case "C":
+                        Console.Clear();
+                        break;
+                    
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+
+            Console.WriteLine("Obrigado por utilizar os nossos serviços");
+        }
+
+        private static void InserirConta()
+        {
+            Console.WriteLine("Inserir nova conta");
             
-            Console.WriteLine(minhaConta.ToString());
+            Console.Write("Digite 1 para Conta Física ou 2 para Jurídica: ");
+            int entradaTipoConta = int.Parse(Console.ReadLine());
+            
+            Console.Write("Digite o Nome do cliente: ");
+            string entradaNome = Console.ReadLine();
+            
+            Console.Write("Digite o saldo inicial: ");
+            double entradaSaldo = double.Parse(Console.ReadLine());
+            
+            Console.Write("Digite o credíto: ");
+            double entradaCredito = double.Parse(Console.ReadLine());
+
+            Conta novaConta = new Conta(tipoConta: (TipoConta) entradaTipoConta,
+                                        saldo: entradaSaldo,
+                                        credito: entradaCredito,
+                                        nome: entradaNome);
+
+            listContas.Add(novaConta);
+
+
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Informe a opção desejada");
+
+            Console.WriteLine("1- Listar contas");
+            Console.WriteLine("2- Inserir nova conta");
+            Console.WriteLine("3- Transferir");
+            Console.WriteLine("4- Sacar");
+            Console.WriteLine("5- Depositar");
+            Console.WriteLine("C- Limpar tela");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
         }
     }
 }
